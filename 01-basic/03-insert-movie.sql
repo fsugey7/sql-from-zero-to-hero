9d2edaa25c2b131fd44fcf5a9252bceb7d5f5a9c
Sugey <sugey.flores@soymas.cl>
2023-09-07 12:24:16 -0300

    sql

---
+++ 01-basic/03-insert-movie.sql
@@ -0,0 +1,14 @@
+\c movies 
+INSERT INTO movies (genere,title,year,director) VALUES('terror','los pitufos',2011,'gargamel');
+
+INSERT INTO movies(genere,title,year,director) VALUES('animacion','la sirenita',1990, 'flounder');
+
+INSERT INTO movies(genere,title,year,director) VALUES('romance','la bella y la bestia',2012,'bestia');
+
+INSERT INTO movies(genere,title,year,director) VALUES('comedia','son como niños',2010,'adam sandler');
+
+INSERT INTO movies(genere,title,year,director) VALUES('comedia','garfield',2004,'oddie');
+
+INSERT INTO movies(genere,title,year,director) VALUES('fantasia','matilda',1996,'mati');
+
+INSERT INTO movies(genere,title,year,director) VALUES('animacion','paw patrol',2021,'perrito');
